bdf0015b46301753c8c6d28527f83c2b0929b793
yjiao <jiaoyangchaming@gmail.com>
2017-01-19 10:27:32 +0800

    dpl fix + ver
    2 cards fixed by dpl
    this version is consistent with 1.967d by sama.

--- script/c7001101.lua
+++ script/c7001101.lua
@@ -3,7 +3,7 @@ function c7001101.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(7001101,0))
-	e1:SetCategory(CATEGORY_COIN)
+	e1:SetCategory(CATEGORY_COIN+CATEGORY_DESTROY+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c7001101.tg)
@@ -13,8 +13,7 @@ end
 function c7001101.filter(c)
 	return c:IsDestructable() or c:IsAbleToHand() 
 end
-function c7001101.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c7001101.filter(chkc) and chkc~=e:GetHandler() end
+function c7001101.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingTarget(c7001101.filter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_COIN,g,1,0,0)
 end
@@ -22,11 +21,11 @@ function c7001101.activate(e,tp,eg,ep,ev,re,r,rp)
 	local res=Duel.TossCoin(tp,1)
 	if res==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local g1=Duel.SelectMatchingCard(tp,c7001101.filter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
-		Duel.Destroy(g1,REASON_EFFECT)
+		local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
+		Duel.Destroy(g,REASON_EFFECT)
 	else 
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local g2=Duel.SelectMatchingCard(tp,c7001101.filter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
-		Duel.SendtoHand(g2,nil,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
 end
